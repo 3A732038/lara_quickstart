8dbc755795aad3486b10b36abd69ce0bff4cbeda
Wu Yuzhe <a0903153387@yahoo.com.tw>
2020-11-02 18:17:32 +0800

    顯示所有任務於tasks視圖上

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,28 @@ use Illuminate\Http\Request;
 */
 //顯示所有任務的清單
 Route::get('/', function () {
-    return view('tasks');
-});
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);});
 // 增加新的任務
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
